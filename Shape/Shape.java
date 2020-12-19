33fdce4dff5278c36cd6f14ebac081059a9220d6
semih-git <cemcan34355@gmail.com>
2020-12-19 23:50:14 +0100

    commend lines added

--- Shape/Shape.java
+++ Shape/Shape.java
@@ -1,11 +1,11 @@
 package Shape;
 
 public abstract class Shape {
-
+    //abstract class field lari
     int boyut1;
     int boyut2;
     int height;
-
+    //constructors (uc,iki ve bir parametre alabilen)
     public Shape(int boyut1, int boyut2, int height) {
         this.boyut1 = boyut1;
         this.boyut2 = boyut2;
@@ -20,7 +20,7 @@ public abstract class Shape {
     public Shape(int boyut1) {
         this.boyut1 = boyut1;
     }
-
+    //abstract class a ait abstract metodlar (bu class i extend eden class larin bu metodlari @override etmesi sart.)
     public abstract void draw();
     public abstract void erase();
     public abstract double calculateArea();
